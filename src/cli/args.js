b0a660ebaf5067fb8f719a63e98581dba9079148
Valerie Ivashkevich <ivashkevich03@gmail.com>
2024-10-01 20:17:19 +0300

    feat: add cli tasks solutions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argsArr = process.argv.slice(2);
+  const argsObj = {};
+  for (let i = 0; i < argsArr.length; i += 2) {
+    argsObj[argsArr[i]] = argsArr[i + 1];
+  }
+
+  let argsArr2 = [];
+  Object.keys(argsObj).forEach((key) =>
+    argsArr2.push(`${key} is ${argsObj[key]}`)
+  );
+  console.log(argsArr2.join(", ").trim());
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  let rssEnvsArr = [];
+
+  Object.keys(process.env).forEach((envKey) => {
+    if (envKey.startsWith("RSS_")) {
+      rssEnvsArr.push(`${envKey}=${process.env[envKey]};`);
+    }
+  });
+
+  console.log(rssEnvsArr.join(" "));
 };
 
 parseEnv();

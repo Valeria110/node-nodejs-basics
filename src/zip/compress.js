e2dea7fe6ccfc2a61369f0be7bd143c0a54489fd
Valerie Ivashkevich <ivashkevich03@gmail.com>
2024-10-01 20:24:35 +0300

    feat: add zip tasks solutions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import path from "path";
+import { pipeline } from "stream";
+import { createGzip } from "zlib";
+
+const dirname = import.meta.dirname;
+
 const compress = async () => {
-    // Write your code here 
+  const readable = fs.createReadStream(
+    path.join(dirname, "files", "fileToCompress.txt")
+  );
+  const writable = fs.createWriteStream(
+    path.join(dirname, "files", "archive.gz")
+  );
+
+  pipeline(readable, createGzip(), writable, (err) => {
+    if (err) throw new Error(err);
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import path from "path";
+import { pipeline } from "stream";
+import { createGunzip } from "zlib";
+
+const dirname = import.meta.dirname;
+
 const decompress = async () => {
-    // Write your code here 
+  const readable = fs.createReadStream(
+    path.join(dirname, "files", "archive.gz")
+  );
+  const writable = fs.createWriteStream(
+    path.join(dirname, "files", "fileToCompress.txt")
+  );
+
+  pipeline(readable, createGunzip(), writable, (err) => {
+    if (err) throw new Error(err);
+  });
 };
 
 await decompress();

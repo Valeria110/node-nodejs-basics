c509346c08edc4ff9ecea36defb19492800d1be6
Valerie Ivashkevich <ivashkevich03@gmail.com>
2024-10-01 20:19:42 +0300

    feat: add hash task solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import { createHash } from "crypto";
+import fs from "fs";
+import path from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const dirname = import.meta.dirname;
+  const readable = fs.createReadStream(
+    path.join(dirname, "files", "fileToCalculateHashFor.txt")
+  );
+  const hash = createHash("sha256");
+  readable.pipe(hash).setEncoding("hex").pipe(process.stdout);
 };
 
 await calculateHash();

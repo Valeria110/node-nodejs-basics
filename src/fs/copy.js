22c009e25a4590ea9ef9c617c7d7475bba0e2cc1
Valerie Ivashkevich <ivashkevich03@gmail.com>
2024-10-01 20:15:19 +0300

    feat: add fs tasks solutions

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,44 @@
+import { join } from "path";
+import fs, { access, constants } from "fs";
+
+const dirname = import.meta.dirname;
+const filesPath = join(dirname, "files");
+const filesCopyPath = join(dirname, "files_copy");
+
 const copy = async () => {
-    // Write your code here 
+  access(filesPath, constants.F_OK, (err) => {
+    if (err) throw new Error("Folder files does not exist");
+
+    access(filesCopyPath, constants.F_OK, async (err2) => {
+      if (!err2) throw new Error("Folder files_copy already exists");
+
+      await createFilesCopyFolder();
+    });
+  });
+};
+
+const createFilesCopyFolder = async () => {
+  fs.mkdir(filesCopyPath, (err) => {
+    if (err) throw new Error(err);
+
+    fs.readdir(filesPath, (err2, files) => {
+      if (err2) throw new Error(err2);
+
+      files.forEach((file) => {
+        fs.readFile(
+          join(filesPath, file),
+          { encoding: "utf-8" },
+          (err3, data) => {
+            if (err3) throw new Error(err3);
+
+            fs.writeFile(join(filesCopyPath, file), data, (err4) => {
+              if (err4) throw new Error(err4);
+            });
+          }
+        );
+      });
+    });
+  });
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import { join } from "path";
+import { access, writeFile, constants } from "fs";
+
+const dirname = import.meta.dirname;
+
 const create = async () => {
-    // Write your code here 
+  const filepath = join(dirname, "files", "fresh.txt");
+  access(filepath, constants.F_OK, (err) => {
+    if (!err) {
+      throw new Error("FS operation failed");
+    }
+
+    writeFile(
+      filepath,
+      "I am fresh and young",
+      { encoding: "utf-8" },
+      (err2) => {
+        if (err2) {
+          throw new Error(err2);
+        }
+      }
+    );
+  });
 };
 
 await create();
